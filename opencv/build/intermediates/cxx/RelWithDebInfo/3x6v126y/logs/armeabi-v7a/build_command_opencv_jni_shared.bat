69aa3e20a2e8d73c9b00e61d2c8ad20a5034ec79
emboss <emboss33@naver.com>
2025-05-23 14:22:07 +0900

    디버깅 로직 추가됨

--- opencv/build/intermediates/cxx/RelWithDebInfo/3x6v126y/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\jongm\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\kotlinfolder\\pthotoditor2\\opencv\\.cxx\\RelWithDebInfo\\3x6v126y\\armeabi-v7a" ^
-  opencv_jni_shared
